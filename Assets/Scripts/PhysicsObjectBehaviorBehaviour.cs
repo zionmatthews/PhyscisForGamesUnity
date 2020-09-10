86e56edbeea77917e6a57a0581f7a40213f6a249
zion matthews <zman5682@gmail.com>
2020-09-10 17:43:59 -0500

    Finally commited

---
+++ Assets/Scripts/PhysicsObjectBehaviorBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsObjectBehavior : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material asleepMaterial = null;
+
+    private Rigidbody _rigidbody = null;
+    private Material _material = null;
+
+    private bool wasAsleep = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.IsSleeping() && !wasAsleep && asleepMaterial != null)
+        {
+            wasAsleep = true;
+            _material = asleepMaterial;
+        }
+        else if (!_rigidbody.IsSleeping() && wasAsleep && awakeMaterial != null)
+        {
+            wasAsleep = false;
+            _material = asleepMaterial;
+        }
+    }
+}
